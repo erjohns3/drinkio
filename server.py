7d398a8042e482d07dd9ff7ddbbbdb70b4de40b6
Eric <johnson.eric321@gmail.com>
2021-05-25 06:55:53 -0400

    stuff

--- server.py
+++ server.py
@@ -23,14 +23,16 @@ tilt_down = 485000
 #####################################
 
 loc = pathlib.Path(__file__).parent.absolute()
+print(loc)
+print(os.path.dirname(__file__))
 
-f = open(loc+"/config.json", "r")
+f = open(str(loc)+"/config.json", "r")
 config = json.loads(f.read())
 f.close()
 
-drinks = config.drinks
-ports = config.ports
-ingredients = config.ingredients
+drinks = config["drinks"]
+ports = config["ports"]
+ingredients = config["ingredients"]
 
 #################################### gpio signals
 
@@ -39,15 +41,18 @@ FLOW_TIMEOUT = 8
 flow_lock = threading.Lock()
 flow_pin = 17
 flow_tick = 0
+flow_zero = 0
 flow_mult = 0.00007514222
 
 def flow_rise(channel):
+    global flow_tick
+    global flow_zero
     flow_lock.acquire()
     flow_tick = flow_tick + 1
     flow_zero = 0
     flow_lock.release()
 
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(flow_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.add_event_detect(flow_pin, GPIO.RISING, callback=flow_rise)
 
@@ -59,10 +64,10 @@ while True:
 
     if drink in drinks:
         for ingredient in drinks[drink]:
-            print("{}, {}".format(ingredient, ports[ingredients[ingredient].port]))
+            print("{}, {}".format(ingredient, ports[ingredients[ingredient]["port"]]))
             pi.hardware_PWM(tilt_pin, 333, tilt_up)
             time.sleep(1)
-            duty = ports[ingredients[ingredient].port]
+            duty = ports[ingredients[ingredient]["port"]]
             if duty < 500000:
                 pi.hardware_PWM(pan_pin, 333, 750000)
             else:
