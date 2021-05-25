054c541d574dbcede5057fc356960c45ad9fa95d
Eric Johnson <johnson.eric321@gmail.com>
2021-05-24 21:53:39 -0400

    stuff

--- server.py
+++ server.py
@@ -1,60 +1,47 @@
 import threading
 import subprocess
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
 import datetime
 import os
 import sys
 import json
-
-#################################### gpio signals
-
-def onRise(channel):
-    print("rise: ", time.time())
-    sys.stdout.flush()
-
-#GPIO.setmode(GPIO.BOARD)
-#GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-#GPIO.add_event_detect(11, GPIO.RISING, callback=onRise, bouncetime=200)
-#GPIO.add_event_detect(11, GPIO.FALLING, callback=onFall, bouncetime=200)
-
-#################################### text to speech
+import pathlib
 
 single = 0.6
 pump_pin = 21
-
-info = {
-    "vodka": {
-        "abv": 0.4
-    }
-}
-
 pins = [25, 8, 7, 1, 12, 16, 20]
 
-valves = {
-    "vodka": 0,
-    "rum": 1,
-    "whiskey": 2,
-    "tequila": 3,
-    "fireball": 4,
-    "orange juice": 5,
-    "coke": 6,
-    "ginger beer": 7,
-    "club soda": 8,
-    "lime": 9,
-    "lemon": 10
-}
+#####################################
 
-drinks = {
-    "Screwdriver": {
-        "vodka": 1.5,
-        "orange juice": 3
-    },
-    "Margarita": {
-        
-    }
+loc = pathlib.Path(__file__).parent.absolute()
 
-}
+f = open(loc+"/info.json", "r")
+info = json.loads(f.read())
+f.close()
+
+f = open(loc+"/valves.json", "r")
+valves = json.loads(f.read())
+f.close()
+
+f = open(loc+"/drinks.json", "r")
+drinks = json.loads(f.read())
+f.close()
+
+#################################### gpio signals
+
+flow_pin = 24
+flow_tick = 0
+flow_mult = 0.00007514222
+
+def flow_rise(channel):
+    flow_tick = flow_tick + 1
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(flow_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(flow_pin, GPIO.RISING, callback=flow_rise)
+
+####################################
 
 
 while True:
@@ -64,6 +51,14 @@ while True:
         for ingredient in drinks[drink]:
             if ingredient in valves:
                 print("{}, {}, {}".format(ingredient, valves[ingredient], pins[valves[ingredient]]))
-
-    #GPIO.output(12, GPIO.HIGH)
-
+                GPIO.output(pins[valves[ingredient]], GPIO.HIGH)
+                time.sleep(0.5)
+                GPIO.output(pump_pin, GPIO.HIGH)
+                pump_state = True
+                flow_tick = 0
+                while flow_tick * flow_mult < drinks[drink][ingredient]:
+                    time.sleep(0.01)
+                GPIO.output(pump_pin, GPIO.LOW)
+                time.sleep(2)
+                GPIO.output(pins[valves[ingredient]], GPIO.LOW)
+                time.sleep(0.5)
