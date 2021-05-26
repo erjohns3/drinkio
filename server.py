41cc7117d1d2e3abad2a5382d3de09ec75159f7a
Eric Johnson <johnson.eric321@gmail.com>
2021-05-26 02:38:23 -0400

    added pump control

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 import threading
 import subprocess
-import RPi.GPIO as GPIO
 import pigpio
 import time
 import datetime
@@ -13,7 +12,15 @@ pi = pigpio.pi()
 
 single = 0.6
 
+#####################################
+
 pump_pin = 27
+
+pi.set_mode(pump_pin, pigpio.OUTPUT)
+pi.write(pump_pin, 1)
+
+#####################################
+
 pan_pin = 12
 tilt_pin = 13
 
@@ -37,24 +44,21 @@ ingredients = config["ingredients"]
 #################################### gpio signals
 
 FLOW_PERIOD = 0.01
-FLOW_TIMEOUT = 8
+FLOW_TIMEOUT = 5
 flow_lock = threading.Lock()
 flow_pin = 17
 flow_tick = 0
-flow_zero = 0
 flow_mult = 0.00007514222
 
 def flow_rise(channel):
     global flow_tick
-    global flow_zero
     flow_lock.acquire()
     flow_tick = flow_tick + 1
-    flow_zero = 0
     flow_lock.release()
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(flow_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.add_event_detect(flow_pin, GPIO.RISING, callback=flow_rise)
+pi.set_mode(flow_pin, pigpio.INPUT)
+pi.set_pull_up_down(flow_pin, pigpio.PUD_DOWN)
+pi.callback(flow_pin, pigpio.RISING_EDGE, flow_rise)
 
 ####################################
 
@@ -66,15 +70,49 @@ while True:
         for ingredient in drinks[drink]:
             print("{}, {}".format(ingredient, ports[ingredients[ingredient]["port"]]))
             pi.hardware_PWM(tilt_pin, 333, tilt_up)
-            time.sleep(1)
+            time.sleep(2)
             duty = ports[ingredients[ingredient]["port"]]
+            
             if duty < 500000:
                 pi.hardware_PWM(pan_pin, 333, 750000)
             else:
                 pi.hardware_PWM(pan_pin, 333, 250000)
+
             time.sleep(1)
             pi.hardware_PWM(pan_pin, 333, duty)
             time.sleep(3)
             pi.hardware_PWM(tilt_pin, 333, tilt_down)
-            time.sleep(1)
+            time.sleep(2)
             
+            pi.write(pump_pin, 0)
+
+            flow_lock.acquire
+            flow_tick = 0
+            flow_lock.release
+
+            elapsed = 0
+            flow_prev = 0
+
+            while True:
+                flow_lock.acquire
+                if flow_tick * flow_mult > drinks[drink][ingredient]:
+                    break
+                
+                if elapsed > 5:
+                    if flow_tick - flow_prev <= 3:
+                        ingredients[ingredient]["empty"] = True
+                        break
+                    flow_prev = flow_tick
+                    elapsed = 4
+
+                elapsed = elapsed + FLOW_PERIOD
+                flow_lock.release
+                time.sleep(FLOW_PERIOD)
+
+            flow_lock.release
+            pi.hardware_PWM(tilt_pin, 333, tilt_up)
+            time.sleep(1)
+
+        time.sleep(4)
+        pi.write(pump_pin, 1)
+        time.sleep(2)
