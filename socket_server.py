2907b16ec6d76496ff8b07ef1a286afced38a9d0
Chris <caschoener@gmail.com>
2021-07-19 16:33:00 -0400

    virtualenv and bugfixes
    .

--- socket_server.py
+++ socket_server.py
@@ -219,7 +219,7 @@ async def init(websocket, path):
 
         socket_lock.release()
 
-start_server = websockets.serve(init, "192.168.86.47", 8765)
+start_server = websockets.serve(init, "0.0.0.0", 8765)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
