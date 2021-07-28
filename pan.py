ce2dc0a5f348994cd6bd57b67d829f68e9890ce3
Eric Johnson <johnson.eric321@gmail.com>
2021-07-27 22:43:24 -0400

    Update pan.py

--- pan.py
+++ pan.py
@@ -5,7 +5,6 @@ import sys
 import json
 import signal
 import pathlib
-from pynput.keyboard import Listener
 
 pi = pigpio.pi()
 
@@ -40,8 +39,6 @@ ingredients = config["ingredients"]
 
 ####################################
 
-pan_lock = threading.Lock()
-
 pan_curr = int(sys.argv[1])
 pan_goal = int(sys.argv[2])
 if pan_curr <= 100:
@@ -51,27 +48,10 @@ if pan_goal <= 100:
 
 print(pan_goal)
 
-def on_press(key):
-    if key == 'w':
-        pan_lock.aquire()
-        pan_goal = pan_goal + 1000
-        print(pan_goal)
-        pan_lock.release()
-    if key == 's':
-        pan_lock.aquire()
-        pan_goal = pan_goal - 1000
-        print(pan_goal)
-        pan_lock.release()
-
-with Listener(on_press=on_press, on_release=on_release) as listener:
-
-    while True:
-        pan_lock.acquire()
-        if pan_goal > pan_curr:
-            pan_curr = min(pan_curr + (PAN_SPEED * PAN_PERIOD), pan_goal)
-            pi.hardware_PWM(PAN_PIN, 333, int(pan_curr))
-        elif pan_goal < pan_curr:
-            pan_curr = max(pan_curr - (PAN_SPEED * PAN_PERIOD), pan_goal)
-            pi.hardware_PWM(PAN_PIN, 333, int(pan_curr))
-        pan_lock.release()
-        time.sleep(PAN_PERIOD)
+while pan_curr != pan_goal:
+    if pan_goal > pan_curr:
+        pan_curr = min(pan_curr + (PAN_SPEED * PAN_PERIOD), pan_goal)
+    else:
+        pan_curr = max(pan_curr - (PAN_SPEED * PAN_PERIOD), pan_goal)
+    pi.hardware_PWM(PAN_PIN, 333, int(pan_curr))
+    time.sleep(PAN_PERIOD)
